37a9df4eb20c3f4e92f5fe378b3e3eef25c6ec89
pxs-yt <158753404+pxs-yt@users.noreply.github.com>
2024-03-03 19:53:43 +0100

    added new words

--- assets/js/fjalori.js
+++ assets/js/fjalori.js
@@ -209,7 +209,111 @@ const albanianWords = {
       definition: "Specialist me arsim të lartë në fushën e mjekësisë, doktor.",
       example: "Mjek kirurg (psikiatër, neurolog, gjinekolog, pediatër, okulist)."
     },
-    
+    "mjeshter": {
+      origin: "Latine",
+      definition: "Punëtor i përgatitur që ushtron një zeje të caktuar, zejtar. | Punëtor i përgatitur e me përvojë, që bën punë me cilësi të lartë, që u jep përvojën e njohuritë e veta më të rinjve dhe që drejton një repart a një sektor prodhimi.",
+      example: "Mjeshtër rrobaqepës."
+    },
+    "mjeshtër": {
+      origin: "Latine",
+      definition: "Punëtor i përgatitur që ushtron një zeje të caktuar, zejtar. | Punëtor i përgatitur e me përvojë, që bën punë me cilësi të lartë, që u jep përvojën e njohuritë e veta më të rinjve dhe që drejton një repart a një sektor prodhimi.",
+      example: "Mjeshtër rrobaqepës."
+    },
+    "njerk": {
+      origin: "Latine",
+      definition: "Burri i dytë i një gruaje kundrejt fëmijëve që ka ajo me burrin e parë, burri i nënës.",
+      example: "Njerku s'është baba. (Fjalë e urtë)"
+    },
+    "prind": {
+      origin: "Latine",
+      definition: "Nëna dhe babai kundrejt fëmijëve.",
+      example: "Prindër të dashur."
+    },
+    "qytet": {
+      origin: "Latine",
+      definition: "Qendër e madhe banimi, me shumë shtëpi të ndara zakonisht në lagje, me rrugë e me sheshe të rregulluara, ku zhvillojnë veprimtarinë e tyre një varg institucionesh politike, shoqërore, administrative, kulturore etj. dhe ndërmarrjesh industriale, tregtare etj.",
+      example: "Qytet i madh."
+    },
+    "shok": {
+      origin: "Latine",
+      definition: "Njeri me të cilin jemi lidhur me miqësi gjatë jetës, në një punë a në një veprimtari të përbashkët ose në luftë për një çështje të përbashkët; njeri me të cilin kemi pikëpamje, mendime e interesa të përbashkëta, kemi moshë pak a shumë të afërt dhe shkojmë mirë njëri me tjetrin.",
+      example: "Janë shokë të ngushtë."
+    },
+    "ungj": {
+      origin: "Latine",
+      definition: "Vëllai i babait ose i nënës (kundrejt nipit a mbesës), xhaxha, dajë.",
+      example: "E kam ungj."
+    },
+    "faqe": {
+      origin: "Latine",
+      definition: "1- Secila nga dy pjesët anësore të fytyrës së njeriut nga syri deri te nofulla e poshtme dhe nga hunda deri te veshi; secila nga dy anët e turirit të kafshëve. <br> 2- Secila nga anët e një sendi; sipërfaqja anësore e diçkaje; pjesa e përparme e kryesore e një ndërtese, e një sendi etj., pjesa që duket, ballë; parëse.",
+      example: "Laj faqet. | Faqe mali."
+    },
+    "flok": {
+      origin: "Latine",
+      definition: " Secila nga qimet e kokës së njeriut; tërësia e këtyre qimeve.",
+      example: "Flokë të zinj. | Flokë kaçurrelë."
+    },
+    "fytyre": {
+      origin: "Latine",
+      definition: "Pjesa e përparme e kokës së njeriut, që përfshin ballin, faqet, mjekrën, gojën, hundën, sytë dhe veshët; pamja e jashtme e pjesës së përparme të kokës së njeriut; shprehja e jashtme e dikujt, që pasqyrohet në këtë pjesë të trupit.",
+      example: "Tiparet (pjesët) e fytyrës."
+    },
+    "fytyrë": {
+      origin: "Latine",
+      definition: "Pjesa e përparme e kokës së njeriut, që përfshin ballin, faqet, mjekrën, gojën, hundën, sytë dhe veshët; pamja e jashtme e pjesës së përparme të kokës së njeriut; shprehja e jashtme e dikujt, që pasqyrohet në këtë pjesë të trupit.",
+      example: "Tiparet (pjesët) e fytyrës."
+    },
+    "kembe": {
+      origin: "Latine",
+      definition: "Secila nga të dyja gjymtyrët e poshtme të trupit të njeriut; secila nga gjymtyrët e poshtme të trupit të një kafshe a të një shpendi që u shërbejnë atyre për të qëndruar, për të ecur a për të lëvizur; pjesë nga kjo gjymtyrë.",
+      example: "Këmba e djathtë."
+    },
+    "këmbë": {
+      origin: "Latine",
+      definition: "Secila nga të dyja gjymtyrët e poshtme të trupit të njeriut; secila nga gjymtyrët e poshtme të trupit të një kafshe a të një shpendi që u shërbejnë atyre për të qëndruar, për të ecur a për të lëvizur; pjesë nga kjo gjymtyrë.",
+      example: "Këmba e djathtë."
+    },
+    "kofshe": {
+      origin: "Latine",
+      definition: "Pjesa e këmbës së njeriut ose të kafshës nga këllku deri te gjuri.",
+      example: "Kofsha e djathtë."
+    },
+    "kofshë": {
+      origin: "Latine",
+      definition: "Pjesa e këmbës së njeriut ose të kafshës nga këllku deri te gjuri.",
+      example: "Kofsha e djathtë."
+    },
+    "koke": {
+      origin: "Latine",
+      definition: "Pjesa e sipërme e trupit të njeriut që qëndron mbi qafën ose pjesa e përparme a e sipërme e trupit të kafshëve, e rrumbullakët a e zgjatur, ku ndodhen truri, goja, sytë, veshët, hunda, flokët etj.",
+      example: "Më dhemb koka."
+    },
+    "kokë": {
+      origin: "Latine",
+      definition: "Pjesa e sipërme e trupit të njeriut që qëndron mbi qafën ose pjesa e përparme a e sipërme e trupit të kafshëve, e rrumbullakët a e zgjatur, ku ndodhen truri, goja, sytë, veshët, hunda, flokët etj.",
+      example: "Më dhemb koka."
+    },
+    "mendje": {
+      origin: "Latine",
+      definition: "Aftësia e njeriut për të menduar, për të njohur e për të pasqyruar botën, për të arsyetuar e për të gjykuar, që arrihet me anë të veprimtarisë së trurit; arsyeja si veti e njeriut; truri si qendër e veprimtarisë mendore.",
+      example: "Aq ia preu mendja."
+    },
+    "shendet": {
+      origin: "Latine",
+      definition: "Gjendje e mirë fiziologjike e një organizmi, kur të gjitha organet me rëndësi jetësore punojnë mirë dhe nuk janë të prekura nga ndonjë sëmundje; gjendja kur organizmi ka aftësi të plota.",
+      example: "Shëndet i mirë (i plotë)."
+    },
+    "shëndet": {
+      origin: "Latine",
+      definition: "Gjendje e mirë fiziologjike e një organizmi, kur të gjitha organet me rëndësi jetësore punojnë mirë dhe nuk janë të prekura nga ndonjë sëmundje; gjendja kur organizmi ka aftësi të plota.",
+      example: "Shëndet i mirë (i plotë)."
+    },
+    "shpatull": {
+      origin: "Latine",
+      definition: "Secila nga të dy kockat e gjera, rrasike e si trekëndësh, që janë djathtas e majtas pjesës së sipërme të shpinës së njeriut; zakon. pjesa e shpinës rreth këtyre kockave; pjesa e trupit të kafshëve me katër këmbë, ku lidhet këmba e përparme me trupin; kocka e kësaj pjese.",
+      example: "Shpatulla e djathtë."
+    },
   };
   
 
